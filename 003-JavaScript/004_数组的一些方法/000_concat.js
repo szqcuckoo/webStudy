091d4acb95a9e0f6b32c264e6b6c7a2e1eb08506
szqcuckoo <szqcuckoo@163.com>
2022-07-18 22:44:58 +0800

    初识react，state

--- 003-JavaScript/004_数组的一些方法/000_concat.js
+++ 003-JavaScript/004_数组的一些方法/000_concat.js
@@ -1,31 +1,33 @@
 // 一、 创建数组的几种方法
 // 1. 字面量法
-let textArr = [1, 2, 3, 4];
+let textArr = [1, 2, 3, 4]
 // 2. 构造函数法
-let funArr = new Array(1, 2, 3, 4);
-console.log(textArr, funArr);
+let funArr = new Array(1, 2, 3, 4)
+console.log(textArr, funArr)
 
 // 二、静态方法
 // Array.from();
-console.log(Array.isArray(textArr));
-console.log(Array(3));
-console.log(Array.of(3));
+console.log(Array.isArray(textArr))
+console.log(Array(3))
+console.log(Array.of(3))
 
 // 三、数组方法
 // 1. Array.prototype.concat()    // TODO:实质是进行了一次浅拷贝合并
-const arr1 = [1, 2];
-const arr2 = [3, 4];
-console.log(arr1.concat(arr2));
+const arr1 = [1, 2]
+const arr2 = [3, 4]
+console.log(arr1.concat(arr2))
 Array.prototype._concat = function () {
-  let args = Array.from(arguments);
+  let args = Array.from(arguments)
   const arr = JSON.parse(JSON.stringify(this))
-  args.map((item) => {
+  args.map(item => {
     if (Array.isArray(item)) {
-      item.map((val) => arr.push(val));
+      item.map(val => arr.push(val))
     } else {
-      arr.push(item);
+      arr.push(item)
     }
-  });
-  return arr;
-};
-console.log(arr1._concat(arr2));
+  })
+  return arr
+}
+console.log(arr1._concat(arr2))
+
+
